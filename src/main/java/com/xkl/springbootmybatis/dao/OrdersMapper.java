2811fb9f796cc686173c6a753ac56ea890558f2f
lixiaoke1205 <68378935@qq.com>
2020-12-27 17:35:55 +0800

    mybatis

---
+++ src/main/java/com/xkl/springbootmybatis/dao/OrdersMapper.java
@@ -0,0 +1,17 @@
+package com.xkl.springbootmybatis.dao;
+
+import com.xkl.springbootmybatis.model.Orders;
+
+public interface OrdersMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
